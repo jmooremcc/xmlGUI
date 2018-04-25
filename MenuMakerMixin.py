ab27f750f8ba2ae909b9400bb2b0afd25d6c21a7
jmooremcc <jmooremcc@hotmail.com>
2018-04-25 10:28:52 -0400

    Refactored code to eliminate top level if else statement that handled menu tags Also eliminated redundant code involving processing child siblings with creation of processSiblings function

--- MenuMakerMixin.py
+++ MenuMakerMixin.py
@@ -24,11 +24,13 @@ from time import sleep
             id is the name to use for the dynamically created variable name that represents the optionmenu
             dataprovider is the function that will load the options
                if dataprovider is missing, radioitems should be used to provide the options
+            
                
          <radioitem name='' value='' onclick='' default=''/>
             name is the radioitem name
             value is the data to pass as an argument to the onclick function
             onclick is the function to call when the item is clicked and must be in the current scope
+             The onclick function should accept a string parm that represents option selected
             default if present indicates this item is the default item and will display a check mark
         .
         .
@@ -37,12 +39,12 @@ from time import sleep
 """
 
 #Tag Constants
-MENU            = 'menu'
 MENUS           = 'menus'
+MENU            = 'menu'
+MENUBUTTON      = 'menubutton'
 MENUITEM        = 'menuitem'
 OPTIONMENU      = 'optionmenu'
 RADIOITEM       = 'radioitem'
-MENUBUTTON      = 'menubutton'
 SEPARATOR       = 'separator'
 # PACKARGS        = 'packargs'
 # Attribute Constants
@@ -116,6 +118,7 @@ class MenuMakerMixin(object):
                     callback = child.attrib[ONCLICK]
                     kwargs[COMMAND] = self.makeCommand(callback, kwargs[VALUE])
 
+                print("option-kwargs: %s" % kwargs)
                 newMenu.add_radiobutton(**kwargs)
 
                 if DEFAULT in child.attrib:
@@ -137,91 +140,96 @@ class MenuMakerMixin(object):
                 self.processChildElement(optmenu, childopt, radiobuttonoptvar=optvar)
 
 
+    def menuTag(self, parent, elem):
+        print("menu: %s " % elem.tag)
+        menuName = elem.attrib[NAME]
+        newMenu = Menu(parent, tearoff=0)
 
+        if self.topmenu is None:
+            self.topmenu = newMenu
+
+        parent.add_cascade(label=menuName, menu=newMenu)
+        print("menuName: %s" % menuName)
+        if TYPE in elem.attrib:
+            menuType = elem.attrib[type]
+            print("menu type is %s" % menuType)
+        else:
+            pass
+
+        for child in elem:
+            if child.tag == MENU:
+                self.parseMenuTree(newMenu, child)
+
+            elif child.tag == SEPARATOR:
+                newMenu.add_separator()
+
+            elif child.tag == OPTIONMENU:
+                optmenu = Menu(parent, tearoff=0)
+                newMenu.add_cascade(label=child.attrib[NAME], menu=optmenu)
+                self.processOptionMenu(parent, optmenu, child)
+
+            elif child.tag == MENUITEM:
+                self.processChildElement(newMenu, child)
+
+    def processSiblings(self, parent, elem):
+        for child in elem:
+            if child.tag == MENUITEM:
+                self.processChildElement(parent, child)
+            else:
+                self.parseMenuTree(parent, child)
+
+    def menusTag(self, parent, elem):
+        self.processSiblings(parent, elem)
+
+    def menuitemTag(self, parent, elem):
+        pass
+
+    def optionmenuTag(self, parent, elem):
+        menuName = elem.attrib[NAME]
+        optmenu = Menu(parent, tearoff=0)
+        parent.add_cascade(label=menuName, menu=optmenu)
+        self.processOptionMenu(parent, optmenu, elem)
+
+    def menubuttonTag(self, parent, elem):
+        kwargs = elem.attrib
+        imageFile = kwargs[IMAGE]
+        menuName = kwargs[NAME]
+        kwargs[RELIEF] = Tkconstants.__getattribute__(kwargs[RELIEF])
+        kwargs[NAME] = menuName.lower()
+        icon = createIcon(imageFile)
+        kwargs[IMAGE] = icon
+
+        frame = Frame(parent)
+        # mb = Menubutton(frame, bg='sky blue', relief=RIDGE, activebackground='green')
+        mb = Menubutton(frame, **kwargs)
+        mb['text'] = 'XXX'
+        mb.menu = menu = Menu(mb, tearoff=0)
+        mb["menu"] = mb.menu
+        mb["image"] = icon
+        setattr(self, menuName + "Img", icon)
+        mb.config(image=icon)
+
+        self.processSiblings(menu, elem)
+
+        mb.pack(side=LEFT, fill=BOTH)
+        frame.pack(side=TOP, fill=BOTH)
 
     def parseMenuTree(self, parent, elem):
-        if elem.tag == MENU:
-            print("menu: %s " % elem.tag)
-            menuName = elem.attrib[NAME]
-            newMenu = Menu(parent, tearoff=0)
-            # packArgs = elem.find(PACKARGS)
-            # if packArgs is not None:
-            #     kwargs = packArgs.attrib
-            #     for key in kwargs:
-            #         value = kwargs[key]
-            #         kwargs[key]=Tkconstants.__getattribute__(value)
-            #
-            #     newMenu.pack(kwargs)
+        dispatcher = {
+            MENUS: self.menusTag,
+            MENU: self.menuTag,
+            MENUBUTTON: self.menubuttonTag,
+            OPTIONMENU: self.optionmenuTag,
+            MENUITEM: None,
+            RADIOITEM: None,
+            SEPARATOR: None
+        }
+        try:
+            dispatcher[elem.tag](parent, elem)
+        except:
+            raise Exception("tag: %s is not a menu tag" % elem.tag)
 
-            if self.topmenu is None:
-                self.topmenu = newMenu
 
-            parent.add_cascade(label=menuName, menu=newMenu)
-            print("menuName: %s" % menuName)
-            if TYPE in elem.attrib:
-                menuType = elem.attrib[type]
-                print("menu type is %s" % menuType)
-            else:
-                pass
-
-            for child in elem:
-                if child.tag == MENU:
-                    self.parseMenuTree(newMenu, child)
-
-                elif child.tag == SEPARATOR:
-                    newMenu.add_separator()
-
-                elif child.tag == OPTIONMENU:
-                    optmenu = Menu(parent, tearoff=0)
-                    newMenu.add_cascade(label=child.attrib[NAME], menu=optmenu)
-                    self.processOptionMenu(parent, optmenu, child)
-                elif child.tag == MENUITEM:
-                    self.processChildElement(newMenu, child)
-
-        elif elem.tag == MENUS:
-            for child in elem:
-                if child.tag == MENUITEM:
-                    self.processChildElement(parent, child)
-                else:
-                    self.parseMenuTree(parent, child)
-
-        elif elem.tag == OPTIONMENU:
-            menuName = elem.attrib[NAME]
-            optmenu = Menu(parent, tearoff=0)
-            parent.add_cascade(label=menuName, menu=optmenu)
-            self.processOptionMenu(parent, optmenu, elem)
-
-        elif elem.tag == MENUBUTTON:
-            # import Tkconstants
-            kwargs = elem.attrib
-            imageFile = kwargs[IMAGE]
-            menuName = kwargs[NAME]
-            kwargs[RELIEF] = Tkconstants.__getattribute__(kwargs[RELIEF])
-            kwargs[NAME] = menuName.lower()
-            icon = createIcon(imageFile)
-            kwargs[IMAGE] = icon
-
-            frame = Frame(parent)
-            # mb = Menubutton(frame, bg='sky blue', relief=RIDGE, activebackground='green')
-            mb = Menubutton(frame, **kwargs)
-            mb['text']='XXX'
-            mb.menu = menu = Menu(mb, tearoff=0)
-            mb["menu"] = mb.menu
-            mb["image"] = icon
-            setattr(self, menuName+"Img", icon)
-            mb.config(image=icon)
-
-            for child in elem:
-                if child.tag == MENUITEM:
-                    self.processChildElement(menu, child)
-                else:
-                    self.parseMenuTree(menu, child)
-
-            mb.pack(side=LEFT, fill=BOTH)
-            frame.pack(side=TOP, fill=BOTH)
-        else:
-           raise Exception("tag: %s is not a menu tag" % elem.tag)
-           #  self.processChildElement(parent, elem)
 
     def fillOptionValues(self, optvar, optmenu, callback):
         values=['OE2','OE3','LocalHost']
@@ -250,7 +258,7 @@ def createIcon(imgFilename):
 
 if __name__ == "__main__":
     root = Tk()
-    root.geometry("200x100")
+    root.geometry("230x100")
     # icon = createIcon('gearIcon.png')
 
     # frame = Frame(root)
