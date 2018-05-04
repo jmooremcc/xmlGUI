e264a5a4f41f22ba38ad9e97bd136ad03610f9fc
jmooremcc <jmooremcc@hotmail.com>
2018-05-04 11:05:58 -0400

    Revisions to improve maintainability Fixed bug that hard coded in OK button Now we check for a button and retrieve the name to use.

--- GUI_MakerMixin.py
+++ GUI_MakerMixin.py
@@ -12,6 +12,7 @@ FORM = 'form'
 GROUP = 'group'
 CHECKBUTTON = 'checkbutton'
 RADIOBUTTON = 'radiobutton'
+BUTTON = 'button'
 
 #Special Attributes
 ONCLICK = 'onclick'
@@ -22,6 +23,7 @@ GROUPLIST = 'grouplist'
 DEFAULT = 'default'
 VARNAME = 'varname'
 VALUE = 'value'
+TEXT = 'text'
 
 
 
@@ -40,9 +42,9 @@ class GUI_MakerMixin(object):
         :return: A reference to the actual function
         """
         if arg is None:
-            return self.__getattribute__(fnName)
+            return getattr(self, fnName)
         else:
-            return partial(self.__getattribute__(fnName), arg)
+            return partial(getattr(self, fnName), arg)
 
     def xlateArgs(self,kwargs):
         for key in kwargs:
@@ -68,160 +70,244 @@ class GUI_MakerMixin(object):
         frame = self.parseXmlElement(master, element)
         return frame
 
-    def parseXmlElement(self, master, element):
-        framepackargs = None
-        if element.tag == FORM:
-            frame = Tkinter.Frame(master, **element.attrib)
-            if len(element) > 0 and PACK in element[0].tag:
-                framepackargs = self.xlateArgs(element[0].attrib.copy())
-                element._children = element[1:]
+    def extractAttrib(self, elem, attrib):
+        """
+            Extracts an attribute from an element and then removes it from the element.
+            It returns the attribute value
+        :type elem: ET.Element
+        :type attrib: str
+        : return attribute
+        """
+        val = elem.get(attrib)
 
-            packargs = None
+        if val is not None:
+            del(elem.attrib[attrib])
+            return val
 
-            for subelement in element:
-                if len(subelement) > 0 and PACK in subelement[0].tag:
-                    packargs = self.xlateArgs(subelement[0].attrib.copy())
-                    subelement._children = subelement[1:]
+        raise Exception("%s not found in %s" % (attrib, elem.tag))
 
-                widget = self.parseXmlElement(frame, subelement)
+    def extractFunction(self, elem, attrib, run=True):
+        """
+        Extracts and returns a function reference from an element based on an attribute
+        :param elem: ET.Element
+        :param attrib: str
+        :return: object
+        """
+        try:
+            fnName = self.extractAttrib(elem, attrib)
+            if fnName is not None:
+                try:
+                    func = getattr(self, fnName)
+                except:
+                    func = getattr(Tkinter, fnName)
+
+                if run:
+                    return func()
+                else:
+                    return func
+        except:
+            pass
+
+        raise Exception("%s not found in %s" % (attrib,elem.tag))
+
+    def extractPackargs(self, elem):
+        packelem = elem.find(PACK)
+        if packelem is not None:
+            packargs = self.xlateArgs(packelem.attrib.copy())
+            elem.remove(packelem)
+            return packargs
+
+
+    def processForm(self, master, element):
+        frameoptions = self.xlateArgs(element.attrib.copy())
+        frame = Tkinter.Frame(master, **frameoptions)
+        framepackargs = self.extractPackargs(element)
+
+        packargs = None
+
+        for subelement in element:
+            packargs = self.extractPackargs(subelement)
+            widget = self.parseXmlElement(frame, subelement)
+
+            if packargs is None:
+                widget.pack()
+            else:
+                widget.pack(**packargs)
+
+        if framepackargs is None:
+            frame.pack()
+        else:
+            frame.pack(**framepackargs)
+
+        return frame
+
+
+    def processGroup(self, master, element):
+        try:
+            grpname = element.attrib[NAME]
+            del (element.attrib[NAME])
+        except:
+            grpname = None
+
+        try:
+            grplistname = element.attrib[GROUPLIST]
+            del (element.attrib[GROUPLIST])
+        except:
+            grplistname = None
+
+        try:
+            onclickfunc = self.extractAttrib(element, ONCLICK)
+        except:
+            onclickfunc = None
+
+        try:
+            varname = self.extractAttrib(element, VARNAME)
+        except:
+            varname = None
+
+        try:
+            varfunc = self.extractFunction(element, TYPEVAR)
+        except:
+            varfunc = None
+
+        framepackargs = self.extractPackargs(element)
+        frame = Tkinter.Frame(master, **element.attrib)
+
+        cblist = element.findall(CHECKBUTTON)
+        if len(cblist) > 0:
+            for cb in cblist:
+                packargs = self.extractPackargs(cb)
+                widget = self.parseXmlElement(frame, cb)
+                element.remove(cb)
+
+                # index = element._children.index(cb)
+                # del(element._children[index])
 
                 if packargs is None:
                     widget.pack()
                 else:
                     widget.pack(**packargs)
-            return frame
 
-        elif element.tag == GROUP:
-            cblist = None
-            rblist = None
-            grpname = element.attrib[NAME]
-            del (element.attrib[NAME])
-            grplistname = element.attrib[GROUPLIST]
-            del (element.attrib[GROUPLIST])
-            cblist = element.findall(CHECKBUTTON)
-            if ONCLICK in element.attrib:
-                onclickfunc = element.attrib[ONCLICK]
-                del (element.attrib[ONCLICK])
+        else:
+            rblist = element.findall(RADIOBUTTON)
+            if len(rblist) > 0:
+                setattr(self, varname, varfunc)
 
-            varfunc = None
-            varname = None
-
-            if VARNAME in element.attrib:
-                varname = element.attrib[VARNAME]
-                del (element.attrib[VARNAME])
-
-            if TYPEVAR in element.attrib:
-                varfunc = getattr(Tkinter, element.attrib[TYPEVAR])()
-                del(element.attrib[TYPEVAR])
-
-            frame = Tkinter.Frame(master, **element.attrib)
-
-            if len(cblist) > 0:
-                packargs = None
-                for cb in cblist:
-                    if len(cb) > 0 and PACK in cb[0].tag:
-                        packargs = self.xlateArgs(cb[0].attrib.copy())
-                        cb._children = cb[1:]
-                    widget = self.parseXmlElement(frame, cb)
-                    # funcname = self.makeCommand(onclickfunc,widget.config('text')[4])
-                    index = element._children.index(cb)
-                    del(element._children[index])
+                for rb in rblist:
+                    packargs = self.extractPackargs(rb)
+                    widget = self.parseXmlElement(frame, rb)
+                    element.remove(rb)
+                    # index = element._children.index(rb)
+                    # del (element._children[index])
 
                     if packargs is None:
                         widget.pack()
                     else:
                         widget.pack(**packargs)
 
+        if framepackargs is None:
+            frame.pack()
+        else:
+            frame.pack(**framepackargs)
+
+        return frame
+
+    def processEntryOptions(self, element, options):
+        textvar = element.find(TEXTVARIABLE)
+        if textvar is not None:
+            varfunc = getattr(Tkinter, textvar.attrib[TYPEVAR])()
+            varname = textvar.attrib[NAME]
+            setattr(self, varname, varfunc)
+            options[TEXTVARIABLE] = varfunc
+            element.remove(textvar)
+            # index = element._children.index(textvar)
+            # del(element._children[index])
+
+        return options
+
+
+    def processCheckbuttonOptions(self, element, options):
+        optvar = element.find(VARIABLE)
+        if optvar is not None:
+            varfunc = getattr(Tkinter, optvar.attrib[TYPEVAR])()
+            default = False
+            if DEFAULT in optvar.attrib:
+                default = optvar.attrib[DEFAULT]
+
+            if default:
+                varfunc.set(1)
             else:
-                rblist = element.findall(RADIOBUTTON)
-                if len(rblist) > 0:
-                    packargs = None
+                varfunc.set(0)
+
+            varname = optvar.attrib[NAME]
+
+            if ONCLICK in optvar.attrib:
+                funcname = optvar.attrib[ONCLICK]
+                options[COMMAND] = self.makeCommand(funcname, varname)
+
+            setattr(self, varname, varfunc)
+            options[VARIABLE] = varfunc
+            index = element._children.index(optvar)
+            del (element._children[index])
+
+        return options
+
+    def processRadiobuttonOptions(self, element, options):
+        optvar = element.find(VARIABLE)
+        if optvar is not None:
+            varname = optvar.attrib[NAME]
+            # varfunc = getattr(self, varname)
+            varfunc = self.extractFunction(optvar, NAME, run=False)
+
+            default = False
+            if DEFAULT in optvar.attrib:
+                default = optvar.attrib[DEFAULT]
+
+            if default:
+                varfunc.set(1)
+            else:
+                varfunc.set(0)
+
+            if ONCLICK in optvar.attrib:
+                funcname = optvar.attrib[ONCLICK]
+                options[COMMAND] = self.makeCommand(funcname, varname)
+
+            options[VARIABLE] = varfunc
+            element.remove(optvar)
+            # index = element._children.index(optvar)
+            # del (element._children[index])
+
+        return options
 
 
-                    setattr(self,varname, varfunc)
+    def parseXmlElement(self, master, element):
+        framepackargs = None
+        if element.tag == FORM:
+            return self.processForm(master, element)
 
-                    for rb in rblist:
-                        if len(rb) > 0 and PACK in rb[0].tag:
-                            packargs = self.xlateArgs(rb[0].attrib.copy())
-                            rb._children = rb[1:]
-                        widget = self.parseXmlElement(frame, rb)
-
-                        index = element._children.index(rb)
-                        del (element._children[index])
-
-                        if packargs is None:
-                            widget.pack()
-                        else:
-                            widget.pack(**packargs)
-
-            return frame
+        elif element.tag == GROUP:
+            return self.processGroup(master, element)
 
         else:
-            options = element.attrib
-            if len(options) > 0:
-                options = options.copy()
-                if COMMAND in options:
-                    options[COMMAND] = self.makeCommand(options[COMMAND],'OK')
+            options = element.attrib.copy()
+            if len(options) > 0 and element.tag == BUTTON:
+                if TEXT in options:
+                    btnName = options[TEXT]
+
+                    if COMMAND in options:
+                        options[COMMAND] = self.makeCommand(options[COMMAND], btnName)
+                else:
+                    options = self.xlateArgs(options)
 
             if len(element):
                 if element.tag == 'entry':
-                    textvar = element.find(TEXTVARIABLE)
-                    if textvar is not None:
-                        varfunc = getattr(Tkinter, textvar.attrib[TYPEVAR])()
-                        varname = textvar.attrib[NAME]
-                        setattr(self,varname,varfunc)
-                        options[TEXTVARIABLE]= varfunc
-                        index = element._children.index(textvar)
-                        del(element._children[index])
+                    options = self.processEntryOptions(element, options)
 
                 elif element.tag == 'checkbutton':
-                    optvar = element.find(VARIABLE)
-                    if optvar is not None:
-                        varfunc = getattr(Tkinter, optvar.attrib[TYPEVAR])()
-                        default = False
-                        if DEFAULT in optvar.attrib:
-                            default = optvar.attrib[DEFAULT]
-
-                        if default:
-                            varfunc.set(1)
-                        else:
-                            varfunc.set(0)
-
-                        varname = optvar.attrib[NAME]
-
-                        if ONCLICK in optvar.attrib:
-                            funcname=optvar.attrib[ONCLICK]
-                            options[COMMAND] = self.makeCommand(funcname,varname)
-
-                        setattr(self, varname, varfunc)
-                        options[VARIABLE] = varfunc
-                        index = element._children.index(optvar)
-                        del (element._children[index])
+                    options = self.processCheckbuttonOptions(element, options)
 
                 elif element.tag == 'radiobutton':
-                    optvar = element.find(VARIABLE)
-                    if optvar is not None:
-                        # varfunc = getattr(Tkinter, optvar.attrib[TYPEVAR])()
-                        varname = optvar.attrib[NAME]
-                        varfunc = getattr(self, varname)
-
-                        default = False
-                        if DEFAULT in optvar.attrib:
-                            default = optvar.attrib[DEFAULT]
-
-                        if default:
-                            varfunc.set(1)
-                        else:
-                            varfunc.set(0)
-
-                        if ONCLICK in optvar.attrib:
-                            funcname = optvar.attrib[ONCLICK]
-                            options[COMMAND] = self.makeCommand(funcname, varname)
-
-                        # setattr(self, varname, varfunc)
-                        options[VARIABLE] = varfunc
-                        index = element._children.index(optvar)
-                        del (element._children[index])
+                    options = self.processRadiobuttonOptions(element, options)
 
                 for subelement in element:
                     options[subelement.tag] = subelement.text
@@ -248,7 +334,7 @@ class GUI_MakerMixin(object):
 
 if __name__ == '__main__':
     root = Tkinter.Tk()
-    root.geometry("230x150")
+    root.geometry("230x200")
 
     m1 = GUI_MakerMixin(root)
     fr = m1.makeGUI(root, "gui1.xml")
