09a077e1a002c4e702013d311311ab66f1a4899f
jmooremcc <jmooremcc@hotmail.com>
2019-08-22 13:14:28 -0400

    Add Licensing information Added pdf document

--- ExampleFiles/main2GUI.py
+++ ExampleFiles/main2GUI.py
@@ -1,6 +1,23 @@
-# from itunesUtility import sendPlaylist2itunes, TS_decorator
-# from downloader import getTargetdir, downloadUrl
-# from playlistMaker import makePlaylist, folderBrowser
+#
+#       Copyright (C) 2018
+#       John Moore (jmooremcc@hotmail.com)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with XBMC; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#
 from tkinter import messagebox
 from tkinter.constants import *
 import tkinter as tk
